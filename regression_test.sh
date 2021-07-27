c489f6a354f9963001edc6627bb78d300eca7acc
Ceyu Gao <2556539G@student.gla.ac.uk>
2021-07-27 21:36:12 +0800

    chapter 5: tests passed

--- regression_test.sh
+++ regression_test.sh
@@ -14,8 +14,8 @@ fi
 
 TEST_VECTOR_DIR=$WORKDIR/tango_with_django_2_code/progress_tests
 CODE_DIR=rango
-START_CHAPTER=3
-END_CHAPTER=4
+START_CHAPTER=5
+END_CHAPTER=5
 
 for i in $(seq $START_CHAPTER $END_CHAPTER); do
   echo ""
